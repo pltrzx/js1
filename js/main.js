02d9a4685c696f49da299427736e696fb0cf4b0f
pltrzx <150812509+pltrzx@users.noreply.github.com>
2024-01-30 17:38:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let userName = prompt('Введите ваше имя')
+alert(`Привет,${userName}`)
+
+let userAge = prompt('Введите возраст')
+
+let Age10 = +userAge + 10
+alert(`${userAge + 10}`)
+
+console.log(`Привет,${userName}`)
+console.log(`Вам через 10 лет будет,${+userAge + 10}`)
+
+let n1 = +prompt('Введите первое число')
+let n2 = +prompt('Введите второе число')
+alert(`${n1}/ ${n2} = ${n1 / n2}`)
+
